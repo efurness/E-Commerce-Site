36184e9a0824a16274263720288f7f9dbfe1831f
Ellen Furness <elasfern@gmail.com>
2022-05-20 21:36:52 -0600

    addingRoutesCategory

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,13 +4,33 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  });
   // find all categories
   // be sure to include its associated Products
-});
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(res.params.id, {
+      include: [{ model: Product, through: Tag, as: 'category_products '}]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'no category found with this id! '});
+      return;
+    }
+  
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
 router.post('/', (req, res) => {
